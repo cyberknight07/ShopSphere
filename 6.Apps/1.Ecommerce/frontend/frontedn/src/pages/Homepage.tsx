5d4bfe08156b28a3da94c22bfec5b60132d352e3
cyberknight07 <jamss0112@gmail.com>
2026-04-14 01:01:03 +0530

    axios updated

--- 6.Apps/1.Ecommerce/frontend/frontedn/src/pages/Homepage.tsx
+++ 6.Apps/1.Ecommerce/frontend/frontedn/src/pages/Homepage.tsx
@@ -1,13 +1,37 @@
 import { ColumnContainer } from '../components/styled-components/ColumnContainer.styles'
 import HeroSection from '../components/ui-components/HeroSection.tsx';
 import BooksSection from '../components/ui-components/BooksSection.tsx';
+import { useEffect, useState } from 'react';
+import { getAllCategories } from '../utililty/productApis.ts';
 
 
 const Homepage = () => {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(  () => {
+    const fetchCategories = async () => {
+          const data = await getAllCategories();
+
+          setCategories(data);
+        }
+
+    
+    try {
+        fetchCategories();
+    } catch (error) {
+      console.log("Error ", error );
+    }
+  }, []);
+
+
   return (
     <ColumnContainer>
       <HeroSection/>
-      <BooksSection/>
+      {categories.map((category) => {
+        console.log(category)
+        return <BooksSection/>
+      })}
 
     </ColumnContainer>
   )
